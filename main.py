b78286c5b9dd75cd44e055f63b82cf1085b48176
wojea <annowojea@gmail.com>
2022-05-19 16:53:57 +0200

    przed zajeciami

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ from zipfile import Path, ZipFile
 
 from sentinelsat import SentinelAPI, read_geojson, geojson_to_wkt
 from datetime import date
-from dotenv import load_dotenv # take environment variables from .env.
-
+from dotenv import load_dotenv
+from PIL import Image
+import numpy
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -20,16 +21,42 @@ if __name__ == '__main__':
     # api.download('21d68494-012e-4416-b5a4-810959b00d4e')
 
     # search by polygon, time, and Hub query keywords
-    # footprint = geojson_to_wkt(read_geojson('map.geojson'))
-    products = api.query(date=('20211219', date(2021, 12, 29)),
+    footprint = geojson_to_wkt(read_geojson('geojson/geojson.geojson'))
+    products = api.query(footprint, date=('20211219', date(2021, 12, 29)),
                          platformname='Sentinel-2',
-                         cloudcoverpercentage=(0, 30),
-                         limit=1)
+                         cloudcoverpercentage=(0, 30),)
 
-    api.download(next(iter(products)))
+    # products = api.query(footprint, date=('20211219', date(2021, 12, 29)),
+    #                      platformname='Sentinel-2',
+    #                      cloudcoverpercentage=(0, 30),
+    #                      limit=1)
+
+    # print(len(products))
+    #this downloads 1 product
+    #api.download(next(iter(products)))
+
     with ZipFile(f'{products[next(iter(products))]["title"]}.zip', 'r') as satelite_zip:
         list_of_all_subfiles = ZipFile.infolist(satelite_zip)
-        allowed_formats = ['B04_20m.jp2', 'B03_20m.jp2', 'B02_20m.jp2']
+        allowed_formats = ['B04_10m.jp2', 'B08_10m.jp2', 'B8A_20m.jp2', 'B11_20m.jp2']
+        filtered_files = list()
+        for allowed_format in allowed_formats:
+            filtered_files_part = [k for k in list_of_all_subfiles if allowed_format in k.filename]
+            filtered_files.extend(filtered_files_part)
+        opened_files = [satelite_zip.open(filtered_file) for filtered_file in filtered_files]
+        #print(opened_files)
+        image = Image.open(next(iter(opened_files)))
+        image_pixels = numpy.array(image)
+        print(image_pixels)
+
+
+def extract_tile_name_from_prod_title(title):
+    return title.split("_")[-2:][0]
+
+
+def unzip(product):
+    with ZipFile(f'{product["title"]}.zip', 'r') as satelite_zip:
+        list_of_all_subfiles = ZipFile.infolist(satelite_zip)
+        allowed_formats = ['B04_10m.jp2', 'B08_10m.jp2', 'B8A_20m.jp2', 'B11_20m.jp2']
         filtered_files = list()
         for allowed_format in allowed_formats:
             filtered_files_part = [k for k in list_of_all_subfiles if allowed_format in k.filename]
@@ -37,34 +64,3 @@ if __name__ == '__main__':
         opened_files = [satelite_zip.open(filtered_file) for filtered_file in filtered_files]
         print(opened_files)
 
-    # archive = Path(ZipFile(f'{products[next(iter(products))]["title"]}.zip', 'r')).iterdir()
-    # for x in archive:
-    #     xsliced = str(x)[:-1]
-    #     print(xsliced)
-    #     subdir = Path.open(x)
-    #     for y in subdir:
-    #         print(y)
-    # print(archive)
-    # print(next(iter(products)))
-    # print('\n')
-    # print(api.get_product_odata('14a2aa57-167c-49c0-b0bf-7623a8f74247'))
-    # api.download('14a2aa57-167c-49c0-b0bf-7623a8f74247')
-    # api.to_geojson(products)
-
-    # # download all results from the search
-    # api.download_all(products)
-    #
-    # # GeoJSON FeatureCollection containing footprints and metadata of the scenes
-    # api.to_geojson(products)
-    #
-    # # GeoPandas GeoDataFrame with the metadata of the scenes and the footprints as geometries
-    # api.to_geodataframe(products)
-
-    # # Get basic information about the product: its title, file size, MD5 sum, date, footprint and
-    # # its download url
-    # api.get_product_odata(<product_id>)
-    #
-    # # Get the product's full metadata available on the server
-    # api.get_product_odata(<product_id>, full=True)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
