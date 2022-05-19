25af601fe1a6f4ab0acccddf5761d70a739fa241
mdenisiuk98 <m.denisiuk98@gmail.com>
2022-05-19 19:39:40 +0200

    19.05 - wykres wegetacja/wilgotnosc

--- main.py
+++ main.py
@@ -3,13 +3,60 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import os
-from zipfile import Path, ZipFile
+from zipfile import  ZipFile
 
 from sentinelsat import SentinelAPI, read_geojson, geojson_to_wkt
 from datetime import date
 from dotenv import load_dotenv
 from PIL import Image
 import numpy
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def get_pixels_from_files(product_title):
+    with ZipFile(f'{product_title}.zip', 'r') as satelite_zip:
+        list_of_all_subfiles = ZipFile.infolist(satelite_zip)
+        allowed_formats = ['B04_10m.jp2', 'B08_10m.jp2', 'B8A_20m.jp2', 'B11_20m.jp2', 'B02_20m.jp2']
+        filtered_files = list()
+        for allowed_format in allowed_formats:
+            filtered_files_part = [k for k in list_of_all_subfiles if allowed_format in k.filename]
+            filtered_files.extend(filtered_files_part)
+        opened_files = [satelite_zip.open(filtered_file) for filtered_file in filtered_files]
+        #print(opened_files)
+        returned = {}
+        for file in opened_files:
+            name = file.name.split('/')[-1].split('_')[-2]
+            image = Image.open(file)
+            image = image.resize((5490,5490))
+            image_pixels = numpy.array(image)
+            returned[name]=image_pixels
+        print(returned)
+        return returned
+
+def extract_tile_name_from_prod_title(title):
+    return title.split("_")[-2:][0]
+
+
+def unzip(product):
+    with ZipFile(f'{product["title"]}.zip', 'r') as satelite_zip:
+        list_of_all_subfiles = ZipFile.infolist(satelite_zip)
+        allowed_formats = ['B04_10m.jp2', 'B08_10m.jp2', 'B8A_20m.jp2', 'B11_20m.jp2', 'B02_20m.jp2']
+        filtered_files = list()
+        for allowed_format in allowed_formats:
+            filtered_files_part = [k for k in list_of_all_subfiles if allowed_format in k.filename]
+            filtered_files.extend(filtered_files_part)
+        opened_files = [satelite_zip.open(filtered_file) for filtered_file in filtered_files]
+
+def agriculture(pixels_dict):
+    agri = numpy.nan_to_num(((pixels_dict['B08']-pixels_dict['B04'])/(pixels_dict['B08']+pixels_dict['B04'])))
+    return agri
+
+def moisture(pixels_dict):
+    moist = numpy.nan_to_num((pixels_dict['B8A']-pixels_dict['B11'])/(pixels_dict['B8A']+pixels_dict['B11']))
+    return moist
+
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -21,46 +68,34 @@ if __name__ == '__main__':
     # api.download('21d68494-012e-4416-b5a4-810959b00d4e')
 
     # search by polygon, time, and Hub query keywords
-    footprint = geojson_to_wkt(read_geojson('geojson/geojson.geojson'))
+    footprint = geojson_to_wkt(read_geojson('geojson/river_deltas.geojson'))
     products = api.query(footprint, date=('20211219', date(2021, 12, 29)),
                          platformname='Sentinel-2',
                          cloudcoverpercentage=(0, 30),)
-
+    # products= [{"title":"S2A_MSIL2A_20211228T230821_N0301_R015_T49CEN_20211229T021039" }]
     # products = api.query(footprint, date=('20211219', date(2021, 12, 29)),
     #                      platformname='Sentinel-2',
     #                      cloudcoverpercentage=(0, 30),
     #                      limit=1)
 
     # print(len(products))
+    # print(products)
     #this downloads 1 product
-    #api.download(next(iter(products)))
+    productDWN = next(iter(products))
+    #api.download(productDWN)
+    # print(productDWN)
+    # exit(0)
+    pixs = get_pixels_from_files("S2B_MSIL2A_20211227T084249_N0301_R064_T36RUV_20211227T105048")
+    moistIndex = moisture(pixs)
+    vegeIndex= agriculture(pixs)
+    frame = pd.concat([pd.Series(moistIndex.flatten()),pd.Series(vegeIndex.flatten())],axis=1).groupby(0).mean()
+    print(frame)
+    frame.plot()
+    plt.show()
+    #Image.fromarray(vegeIndex,'L').show()
+    #Image.frombytes('L',(5490,5490),vegeIndex).show()
 
-    with ZipFile(f'{products[next(iter(products))]["title"]}.zip', 'r') as satelite_zip:
-        list_of_all_subfiles = ZipFile.infolist(satelite_zip)
-        allowed_formats = ['B04_10m.jp2', 'B08_10m.jp2', 'B8A_20m.jp2', 'B11_20m.jp2']
-        filtered_files = list()
-        for allowed_format in allowed_formats:
-            filtered_files_part = [k for k in list_of_all_subfiles if allowed_format in k.filename]
-            filtered_files.extend(filtered_files_part)
-        opened_files = [satelite_zip.open(filtered_file) for filtered_file in filtered_files]
-        #print(opened_files)
-        image = Image.open(next(iter(opened_files)))
-        image_pixels = numpy.array(image)
-        print(image_pixels)
-
-
-def extract_tile_name_from_prod_title(title):
-    return title.split("_")[-2:][0]
+    
 
 
-def unzip(product):
-    with ZipFile(f'{product["title"]}.zip', 'r') as satelite_zip:
-        list_of_all_subfiles = ZipFile.infolist(satelite_zip)
-        allowed_formats = ['B04_10m.jp2', 'B08_10m.jp2', 'B8A_20m.jp2', 'B11_20m.jp2']
-        filtered_files = list()
-        for allowed_format in allowed_formats:
-            filtered_files_part = [k for k in list_of_all_subfiles if allowed_format in k.filename]
-            filtered_files.extend(filtered_files_part)
-        opened_files = [satelite_zip.open(filtered_file) for filtered_file in filtered_files]
-        print(opened_files)
 
